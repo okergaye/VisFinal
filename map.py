535d008ac85f86ce2842fc4a81c52af14e27e8fb
LINGXI-XPS\Lingxi <lingxizhong@gmail.com>
2019-03-28 12:44:02 -0600

    Got a map to show data 10/10

--- map.py
+++ map.py
@@ -58,3 +58,32 @@ else:
         print("Not a valid year")
         exit(0)
 print(year)
+
+worldmap = os.path.join('./', 'world.json')
+
+# Load the unemployment value of each state
+# Find the original file here: https://github.com/python-visualization/folium/tree/master/examples/data
+measles_disease_path = os.path.join(
+    './', 'measles_disease_data.csv')
+measles_disease_datam = pd.read_csv(measles_disease_path)
+
+# Initialize the map:
+disease_map = folium.Map(location=[37, -102], zoom_start=5)
+
+# Add the color for the chloropleth:
+disease_map.choropleth(
+    geo_data=worldmap,
+    name='choropleth',
+    data=measles_disease_datam,
+    columns=['ISO3', 'Total'],
+    key_on='feature.id',
+    fill_color='YlGn',
+    fill_opacity=0.7,
+    line_opacity=0.2,
+    legend_name='Measle Occurence \# of people',
+    bins = [0, 10, 50, 100, 150, 200, 300, 3000, 50000]
+)
+folium.LayerControl().add_to(disease_map)
+
+# Save to html
+disease_map.save('disease_map.html')
